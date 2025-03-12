6a83339099a9ed3cd810c447dc36f1cb4f794e81
TOMASTELLEZ <t.tellez.isw@unipolidgo.edu.mx>
2025-03-11 23:36:01 -0600

    Add files via upload

---
+++ aca entre nos/js/homepage.js
@@ -0,0 +1,82 @@
+$(document).ready(() => {
+    $('#hamburger-menu').click(() => {
+        $('#hamburger-menu').toggleClass('active')
+        $('#nav-menu').toggleClass('active')
+    })
+
+    // setting owl carousel
+
+    let navText = ["<i class='bx bx-chevron-left'></i>", "<i class='bx bx-chevron-right'></i>"]
+
+    $('#hero-carousel').owlCarousel({
+        items: 1,
+        dots: false,
+        nav:true,
+        navText: navText,
+        autoplay: true,
+        autoplayHoverPause: true
+    })
+
+    $('#top-movies-slide').owlCarousel({
+        items: 2,
+        dots: false,
+        loop: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+            500: {
+                items: 3
+            },
+            1280: {
+                items: 4
+            },
+            1600: {
+                items: 6
+            }
+        }
+    })
+
+    $('.movies-slide').owlCarousel({
+        items: 2,
+        dots: false,
+        nav:true,
+        navText: navText,
+        margin: 15,
+        responsive: {
+            500: {
+                items: 2
+            },
+            1280: {
+                items: 4
+            },
+            1600: {
+                items: 6
+            }
+        }
+    })
+})
+
+// preguntas frecuentes 
+
+document.querySelectorAll('.faq-question').forEach(button => {
+    button.addEventListener('click', () => {
+        const answer = button.nextElementSibling;
+        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const searchIcon = document.getElementById("search-icon");
+    const searchBar = document.getElementById("search-bar");
+    const closeSearch = document.getElementById("close-search");
+
+    // Mostrar la barra de búsqueda
+    searchIcon.addEventListener("click", function () {
+        searchBar.style.display = "flex";
+    });
+
+    // Ocultar la barra de búsqueda
+    closeSearch.addEventListener("click", function () {
+        searchBar.style.display = "none";
+    });
+});
